648d22b8b7bd5b0f89b22b15e8e1c3c7cfa96982
Justin <justintorre75@gmail.com>
2022-09-15 19:32:23 -0700

    initial port over of scripts and initial creation of docker file

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM pytorch/pytorch:1.12.1-cuda11.3-cudnn8-runtime
+COPY weights /weights
+RUN apt-get update
+RUN apt-get install python3.8 git -y
+
+WORKDIR /app
+RUN git clone  https://github.com/bfirsh/stable-diffusion.git
+WORKDIR /app/stable-diffusion
+RUN git checkout 69ae4b35e0a0f6ee1af8bb9a5d0016ccb27e36dc
+
+SHELL ["/bin/bash", "-c"]
+RUN conda env create -f environment.yaml
---
+++ bhunkio.py
@@ -0,0 +1,93 @@
+from dataclasses import dataclass
+from glob import glob
+from scripts.txt2img import txt_to_img, TxtToImgRequest
+import shutil
+
+
+import base64
+from time import sleep
+import requests
+import os
+import urllib
+
+URL = os.environ.get("BHUNKIO_URL")
+KEY = os.environ.get("API_KEY")
+
+
+def build_url(base_url, path, args_dict={}):
+    url_parts = list(urllib.parse.urlparse(base_url))
+    url_parts[2] = path
+    url_parts[4] = urllib.parse.urlencode(args_dict)
+    return urllib.parse.urlunparse(url_parts)
+
+
+def get_image(outdir: str, prompt: str) -> str:
+    request = TxtToImgRequest(
+        prompt=prompt,
+        n_samples=1,
+        n_iter=1,
+        plms=True,
+        precision="full",
+        outdir=outdir,
+        skip_grid=True
+    )
+    print(txt_to_img(request))
+
+
+@dataclass
+class PromptQueue:
+    uuid: str
+    user_id: str
+    prompt: str
+    result_id: str
+    status: int
+    current_worker: str
+    created_at: str
+    job_started_time: str
+
+
+def image_to_binary(path: str) -> str:
+    with open(path, mode='rb') as file:
+        fileContent = file.read()
+        return base64.b64encode(fileContent).decode("utf8")
+
+
+def do_job(prompt_request: PromptQueue):
+    out_dir = f"outputs/txt2img-results/{prompt_request.uuid}"
+    get_image(outdir=out_dir, prompt=prompt_request.prompt)
+    images = glob(f"{out_dir}/samples/*")
+    binary_images = [image_to_binary(i) for i in images]
+
+    shutil.rmtree(out_dir)
+
+    return {
+        "images": binary_images,
+        "prompt_uuid": prompt_request.uuid,
+        "user_uuid": prompt_request.user_id,
+    }
+
+
+def main():
+    jobs = None
+    while True:
+        print("getting jobs..")
+        job_request = requests.get(
+            build_url(URL, "/api/jobs"),
+            headers={'Authorization': KEY}
+        )
+        if job_request and job_request.json().get('uuid') != None:
+            print(f"Got a job! - {job_request.json()}")
+            result = do_job(PromptQueue(**job_request.json()))
+            response = requests.post(
+                build_url(URL, "/api/jobs"),
+                headers={'Authorization': KEY},
+                json=result
+            )
+        else:
+            print(f"no response {job_request.text}")
+            sleep(1)
+            continue
+
+
+if __name__ == "__main__":
+    main()
